e5972a4fcd8f0c54694eb5463eba242e582992ad
ConradoFrasson <conradox12@gmail.com>
2025-11-04 12:33:04 -0300

    images

--- data_provider.py
+++ data_provider.py
@@ -21,7 +21,7 @@ class DataProvider:
         self.battery_capacity_kwh = 85.0
 
     def update(self):
-        # Simulação de RPM e Velocidade
+
         if self._rpm_direction == 1:
             self.rpm += random.randint(150, 250)
             if self.rpm > 11800: self._rpm_direction = -1
@@ -30,7 +30,7 @@ class DataProvider:
             if self.rpm < 1000: self._rpm_direction = 1
         self.speed = int((self.rpm / 12000) * 180)
 
-        # Simulação de Pedais e Delta de Potência
+ 
         if self._rpm_direction == 1 and self.rpm > 1500:
             self.accelerator = int((self.rpm / 12000) * 100)
             self.brake = 0
@@ -40,23 +40,23 @@ class DataProvider:
             self.brake = int(random.uniform(0.2, 0.8) * 100)
             self.power_delta = (5 + (self.speed / 180) * 15)
 
-        # Atualização do SOC
+
         soc_delta = self.power_delta / self.battery_capacity_kwh
         self.soc += (soc_delta / 500)
         self.soc = max(0.0, min(1.0, self.soc))
 
-        # Atualização das Temperaturas
+
         self.battery_temp = 20 + (1 - self.soc) * 40 + (self.speed / 180) * 20
         self.engine_temp = 20 + (self.rpm / 12000) * 90
         
-        # Simulação dos Pneus
+
         for key in self.tyre_data:
             self.tyre_data[key]['temp'] += random.uniform(-0.1, 0.2)
             self.tyre_data[key]['temp'] = max(40, min(100, self.tyre_data[key]['temp']))
 
-        # Simulação de Falhas (mais provável de acontecer)
+        
         if random.random() < 0.005: self.faults["bms"] = not self.faults["bms"]
         if random.random() < 0.005: self.faults["inverter"] = not self.faults["inverter"]
         
-# Singleton instance
+
 data_provider = DataProvider()
